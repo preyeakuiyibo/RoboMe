675c93c957303020cbd979f752cdecb3a3f3764b
preyeakuiyibo <33173140+preyeakuiyibo@users.noreply.github.com>
2020-10-14 00:27:28 -0400

    Updates

---
+++ precache-manifest.b4abf634d0d27770dd530382aeb6f1b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da99ebac4cdcde2e7575ce842ab03992",
+    "url": "/RoboMe/index.html"
+  },
+  {
+    "revision": "71f06a3d60f9194672c4",
+    "url": "/RoboMe/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d4866fcc72afca4a54fa",
+    "url": "/RoboMe/static/css/main.d2ec9b90.chunk.css"
+  },
+  {
+    "revision": "71f06a3d60f9194672c4",
+    "url": "/RoboMe/static/js/2.6c144b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboMe/static/js/2.6c144b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4866fcc72afca4a54fa",
+    "url": "/RoboMe/static/js/main.4a45ff15.chunk.js"
+  },
+  {
+    "revision": "bece65d70798cdd0be1e",
+    "url": "/RoboMe/static/js/runtime-main.3434091d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboMe/static/media/SEGA.12a3db8b.woff"
+  }
+]);
